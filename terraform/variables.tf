adac9eaf8aac590045046f5014e05eb2ea2546b4
Karthik Varma <pujarikarthikvarma@gmail.com>
2025-12-12 11:38:09 +0530

    Ready for fresh deploy after deleting existing resources

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,6 +10,11 @@ variable "ssh_public_key" {
   type = string
 }
 
+variable "public_key_path" {
+  default = "terraform/karthikkey.pub"
+}
+
+
 variable "ami_id" {
   default = "ami-0e387eb44f4f2a0e7"
 }
